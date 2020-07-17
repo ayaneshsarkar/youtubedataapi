cefbfcc2bb0f4b1258d0ded538b6574f5d95fe47
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-07-18 00:06:04 +0530

    Initial Commit :heavy_check_mark:

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'); ?>
+<?php require_once __DIR__ . '/vendor/autoload.php'; ?>
+
+
+<?php 
+
+  $client = new Google_Client();
+  $client->setApplicationName('YouTube Data API');
+  $client->setScopes(['https://www.googleapis.com/auth/youtube.force-ssl']);
+
+  $client->setAuthConfig('./client_secret.json');
+  $client->setAccessType('offline');
+
+  // Request Auth from User
+  $authUrl = $client->createAuthUrl();
+  printf("Open this link in your browser:\n%s\n", $authUrl);
+  print('Enter verification code: ');
+  $authCode = trim(fgets(STDIN));
+
+  //Exchange Auth Code for an Access Token
+  $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+  $client->setAccessToken($accessToken);
+
+
+?>
 
   <!-- Navbar -->
   <nav id="navbar">
@@ -7,8 +31,8 @@
         <a href="#" class="navbar_logo">YOUTUBE API</a>
 
         <div class="navbar_menu">
-          <li class="navbar_menu-list"><a href="#">LOGIN</a></li>
-          <li class="navbar_menu-list"><a href="#">LOGOUT</a></li>
+          <li class="navbar_menu-list"><a id="login" href="#">LOGIN</a></li>
+          <li class="navbar_menu-list"><a id="logout" href="#">LOGOUT</a></li>
         </div>
       </div>
     </div>
@@ -19,8 +43,8 @@
     <div class="container">
       <form action="#" method="POST" class="index-form">
         <div class="inputbox">
-          <label for="search-channel">Search Videos</label>
-          <input type="text" placeholder="Search Videos" name="search-channel" id="channelSearch">
+          <label for="search-video">Search Videos</label>
+          <input type="text" placeholder="Search Videos" name="search-video" id="videoSearch">
         </div>
 
         <div class="input_button">
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
-console.log('It Works!');
+const login = document.getElementById('login');
+const logut = document.getElementById('logout');
+const videoSearch = document.getElementById('videoSearch');
+
+// Hiding the Logout Button
+logout.style.display = 'none';
+
+/**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+*/
+
+const loadClient = async () => {
+
+  // Setting The API & oAUTH URL
+  gapi.client.setApiKey('AIzaSyAth-FzxYIc5PSSuQAomHN5qQS8G8Bly0c');
+  const oauthURL = "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest";
+
+  try {
+    const load = await gapi.client.load(oauthURL);
+    return load;
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+// Authenticate OAuth 2.0
+const authenticate = async () => {
+
+  // Auth URL
+  const authURL = "https://www.googleapis.com/auth/youtube.force-ssl";
+
+  // Getting The Data
+  try {
+    return (await gapi.auth2.getAuthInstance()).signIn({scope: authURL});
+    loadClient();
+  } catch(error) {
+    console.log(error.message);
+  }
+  
+}
+
+
+
+//login.addEventListener('click', authenticate);
