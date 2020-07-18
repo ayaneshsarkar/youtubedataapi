2db9a38df81bf1c68fe981c21f4fc54f8b17c9a1
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-07-18 23:38:18 +0530

    Initial Commit :heavy_check_mark:

--- index.php
+++ index.php
@@ -57,8 +57,8 @@
 
     $queryParams = [
       'maxResults' => 10,
-      'q' => $query,
-      'key' => 'AIzaSyAth-FzxYIc5PSSuQAomHN5qQS8G8Bly0c'
+      'q' => $query
+      //'key' => 'AIzaSyAth-FzxYIc5PSSuQAomHN5qQS8G8Bly0c'
     ];
 
     $youtubeData = $youtubeService->search->listSearch('snippet', $queryParams);
